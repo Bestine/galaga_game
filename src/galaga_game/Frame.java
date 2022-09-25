6ae7fb48f5eb47780804de5a6a630ce72ae9466f
Bestine <bestineokinda@gmail.com>
2022-09-25 17:11:47 +0300

    Created a frame

---
+++ src/galaga_game/Frame.java
@@ -0,0 +1,18 @@
+package galaga_game;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	
+	Frame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+		this.setResizable(false);
+		this.setSize(900, 600);
+		this.setVisible(true);
+		this.getContentPane().setBackground(Color.BLACK);
+	}
+
+}
